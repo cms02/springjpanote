44a4812d303115bcf6063bdaa3d55ef0d65289b2
cms02 <minoui1234@gmail.com>
2022-05-15 12:25:13 +0900

    주문서비스 개발 (+ 주문 검색 기능)

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -12,12 +12,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class orderService {
+public class OrderService {
 
     private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
